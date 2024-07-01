b00b31f3e4ba992af9a5dbcf181701a3f125d3c6
Camille L <camille0756@gmail.com>
2024-07-01 23:51:06 +0200

    favorite under comments

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -8,10 +8,11 @@ interface FavoriteButtonProps {
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
 
-    const icon = isFavorite ? FaHeartCircleCheck : FaHeartCirclePlus;
+    //const icon = //isFavorite ? FaHeartCircleCheck : FaHeartCirclePlus;
+    //icon instead of button
     return (
         <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-            <icon />
+            <button />
         </div>
     )
 }
