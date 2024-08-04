582a45546c44c2fb863e6b2b234cef813f381bd8
Camille L <camille0756@gmail.com>
2024-08-04 23:34:03 +0200

    set FaInfo button on cards

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React, { lazy } from 'react';
 import Movie from '@/models/Movie';
 import { BsFillPlayFill } from 'react-icons/bs';
+import { FaInfo } from "react-icons/fa6";
 import FavoriteButton from '@/components/FavoriteButton';
 
 interface MovieCardProps {
@@ -21,11 +22,15 @@ const MovieCard = ({ movie }: MovieCardProps) => {
                 src={movie.thumbnailUrl} alt="Thumbnail" loading="lazy" />
                 <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
                     <div className="flex flex-row items-center gap-3">
-                        <div className="cursor-pointer text-black w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+                        <button className="cursor-pointer text-black w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
                         onClick={play}>
                             <BsFillPlayFill size={30} />
-                        </div>
+                        </button>
                         <FavoriteButton movieId={movie?.id}/>
+                        <button className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+                        onClick={play}>
+                            <FaInfo size={15} />
+                        </button>
                     </div>
                     <p className="text-green-400 font-semibold mt-4">
                         New <span className="text-white">2024</span>
